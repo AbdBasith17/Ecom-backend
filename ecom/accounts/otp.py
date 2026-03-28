12dc4612d59445bd864e87b04f9ff24d1c5ffe91
Abdul Basith <abzit377@gmail.com>
2026-03-28 16:25:23 +0530

    smtp fix

--- ecom/accounts/otp.py
+++ ecom/accounts/otp.py
@@ -2,9 +2,9 @@ from django.core.mail import send_mail
 
 def send_otp_email(email, otp):
     send_mail(
-        subject="OTP Verification for PerfAura",
+        subject="OTP Verification for Perfaura",
         message=f"Your OTP is {otp}",
-        from_email="basipp123@example.com",
+        from_email="basipp123@gmail.com",
         recipient_list=[email],
         fail_silently=False
     )
